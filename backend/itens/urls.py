7483b3ad864b8c2fecd7fe0a95017bdb0717be71
Helen <helen.christine.kds@gmail.com>
2025-11-03 15:48:53 -0300

    Python best pratices and remove basename parameter

--- backend/itens/urls.py
+++ backend/itens/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ItemViewSet
 
 router = DefaultRouter()
-router.register(r'itens', ItemViewSet, basename='itens')
+router.register(r'itens', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
