afb761422c8fff62f402cd2c550954aa7e561fcf
Helen <helen.christine.kds@gmail.com>
2025-11-05 17:59:30 -0300

    Remove aspas simples da propriedade fontFamily no CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import ItemForm from './components/ItemForm';
 
 function App() {
     return (
-        <div style={{ padding: '20px', 'fontFamily': 'sans-serif'}}>
+        <div style={{ padding: '20px', fontFamily: 'sans-serif'}}>
             <h1>🛍️ Lista de Compras</h1>
             <ItemForm onAdd={() => window.location.reload()} />
             <ItemList />
